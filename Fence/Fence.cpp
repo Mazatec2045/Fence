51446f35d07d5a9625066ee07dfbb8e8504c7435
Omar Vergara <omar.vergara@my.tccd.edu>
2024-02-05 12:49:59 -0600

    Update code

--- Fence/Fence.cpp
+++ Fence/Fence.cpp
@@ -1,8 +1,27 @@
 
-
+#include <cmath>
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Constants
+    const double squareFeetPerGallon = 340.0;
+    const int numberOfCoats = 2;
+
+    // Dimensions of the wooden fence
+    const int fenceHeight = 6;
+    const int fenceLength = 100;
+
+    // Calculate total square feet to be painted
+    double totalSquareFeet = numberOfCoats * 2.0 * fenceHeight * fenceLength;
+
+    // Calculate gallons of paint needed
+    double gallonsNeeded = (totalSquareFeet / squareFeetPerGallon);
+
+    // Display the result
+    cout << "To paint " << numberOfCoats << " coats on " << totalSquareFeet << " square feet of fence, you will need "
+        << gallonsNeeded << " gallons of paint.\n" ;
+
+    return 0;
 }
